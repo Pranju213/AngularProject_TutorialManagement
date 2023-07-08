31247e1a355da37b030b2d2cfc282c60da2cfa19
Prajkta Shinde <114865564+Pranju213@users.noreply.github.com>
2023-07-08 17:31:20 +0530

    Add files via upload

---
+++ spring-boot-server.zip_expanded/spring-boot-server/src/main/java/com/spring/datajpa/SpringBootDataJpaApplication.java
@@ -0,0 +1,14 @@
+package com.spring.datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaApplication.class, args);
+		System.out.println("Tutorial App is Running....");
+	}
+
+}
